58321335e637035ac04874d70d685918db32d417
kssb2156 <kssb2156@gmail.com>
2025-11-05 15:23:34 +0700

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // ข้อมูลรายการชำระเงินทั้งหมด (ใช้ปี พ.ศ. 256x)
 const allPayments = [
+    { date: '05/11/2568', amount: 2000, description: 'ชำระเงิน', year: 2568, slip: 'https://s.imgz.io/2025/11/05/imagee4e3d50fd8ecc4f8.png' },
     { date: '05/10/2568', amount: 2000, description: 'ชำระเงิน', year: 2568, slip: 'https://s.imgz.io/2025/10/05/imageee1c0defc956e287.png' },
     { date: '05/09/2568', amount: 2000, description: 'ชำระเงิน', year: 2568, slip: 'https://s.imgz.io/2025/09/10/5Sep6876dea1385ac5a259.jpg' },
     { date: '06/08/2568', amount: 2000, description: 'ชำระเงิน', year: 2568, slip: 'https://s.imgz.io/2025/09/10/6Aug6826a4e7bb31ac56ea.jpg' },
